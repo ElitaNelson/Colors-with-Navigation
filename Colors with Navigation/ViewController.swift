6305e8b640cbb989b1c7e9f26cd356aaf4309ca1
Elita Nelson <ezn526@mail.missouri.edu>
2019-11-07 21:13:19 -0600

    First Commit

---
+++ Colors with Navigation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Colors with Navigation
+//
+//  Created by Elita Nelson on 11/7/19.
+//  Copyright © 2019 Elita Nelson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
